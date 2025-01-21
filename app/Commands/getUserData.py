3b719d6538d9f189ed2400679a1636bbc8188728
lpinqwe <gorelikovva@gmail.com>
2025-01-21 21:04:31 +0100

    getUserData change

--- app/Commands/getUserData.py
+++ app/Commands/getUserData.py
@@ -26,6 +26,7 @@ class getUserData(Command):
             customfeedback = "{" + f'''
                     "purpose":"{pur}",
                     "consumers":["{user}"],
+                    "username": "{tmp[0]}",
                     "nickname": "{tmp[2]}",
                     "phonenumber": "{tmp[3]}",
                     "userkey": "{tmp[4]}",
