5ff912658c9f765b0fe04c5881fa11ba56647315
lpinqwe <gorelikovva@gmail.com>
2025-01-22 00:50:39 +0100

    change

--- app/Commands/newMessage.py
+++ app/Commands/newMessage.py
@@ -17,6 +17,7 @@ class newMessage(Command):
     sendTime:ZonedDateTime
     должно добавить сообщение в базу
     """
+
     def __init__(self, body_json):
         self.body_json = body_json
         self.connection = DBReader()
@@ -27,34 +28,33 @@ class newMessage(Command):
             command_entity = json.loads(self.body_json)
             pur = command_entity['purpose']
             user = command_entity['username']
-            messageId=command_entity['messageId']
-            chatId=command_entity['chatId'] #########
-            text=command_entity['text']
-            IsAnswer=command_entity['isAnswer']
-            isDeleted=command_entity['isDeleted']
-            isRead=command_entity['isRead']
-            sendTime=command_entity['sendTime']
+            messageId = command_entity['messageId']
+            chatId = command_entity['chatId']  #########
+            text = command_entity['text']
+            IsAnswer = command_entity['isAnswer']
+            isDeleted = command_entity['isDeleted']
+            isRead = command_entity['isRead']
+            sendTime = command_entity['sendTime']
 
             sql = """
             INSERT INTO public.messages (msgid, chatid, text, isdeleted, isread, sendtime, isanswer, fromuid) 
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             """
-            params = [messageId,chatId,text,isDeleted,isRead,sendTime,IsAnswer,user]
+            params = [messageId, chatId, text, isDeleted, isRead, sendTime, IsAnswer, user]
             for i in range(len(params)):
                 if params[i] == '':
                     params[i] = None
 
-
-            self.connection.read_data(sql,params)
+            self.connection.read_data(sql, params)
 
             sql = """
             select userid 
             from public.chatandusers 
             where chatid = %s;            
             """
-            params=[chatId]
-            tmp=self.connection.read_data(sql,params)
-            #print(tmp)
+            params = [chatId]
+            tmp = self.connection.read_data(sql, params)
+            # print(tmp)
             print("TMP->")
             print([row[0] for row in tmp])
             data = {
@@ -63,12 +63,12 @@ class newMessage(Command):
                 "messageId": messageId,
                 "chatId": chatId,
                 "text": text,
-                "sender":user,
-                "isAnswer":IsAnswer,
+                "sender": user,
+                "isAnswer": IsAnswer,
                 "isDeleted": isDeleted,
                 "isRead": isRead,
-                "sendTime":sendTime
-                }
+                "sendTime": sendTime
+            }
             return json.dumps(data)
         except Exception as e:
             print(e)
